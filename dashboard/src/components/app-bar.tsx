17a9d28ba2180c3c445077702985725a2bf75b0e
Jay-Karia <jay.sanjay.karia@gmail.com>
2025-10-11 02:49:00 +0530

    chore: add search bar in the app bar in dashboard

--- dashboard/src/components/app-bar.tsx
+++ dashboard/src/components/app-bar.tsx
@@ -1,6 +1,8 @@
 import Logo from "./logo";
-// import Search from "./ui/search";
+import { Button } from "./ui/button";
+import Search from "./ui/search";
 import { ThemeToggle } from "./ui/theme-toggle";
+import { IoSettingsOutline } from "react-icons/io5";
 
 export default function AppBar() {
 
@@ -8,13 +10,21 @@ export default function AppBar() {
     <div className="h-16 bg-base-100 px-4 flex items-center justify-around border-b border-gray-200 dark:border-gray-700">
       <div className="flex items-center gap-2">
         <Logo />
-        <h4 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+        <h4 className="scroll-m-20 text-2xl font-medium tracking-tight text-gray-600 dark:text-gray-400">
           Where Was I
         </h4>
       </div>
-      <div></div>
       <div>
+        <Search />
+      </div>
+      <div className="flex items-center gap-2">
+        {/* Theme Toggle */}
         <ThemeToggle />
+
+        {/* Settings */}
+        <Button variant="ghost" size="icon">
+          <IoSettingsOutline size={20} />
+        </Button>
       </div>
     </div>
   );
--- dashboard/src/components/ui/search.tsx
+++ dashboard/src/components/ui/search.tsx
@@ -1,26 +1,19 @@
 import { useState } from "react";
 import { CiSearch } from "react-icons/ci";
+import { Input } from "./input";
 
 export default function Search() {
     const [query, setQuery] = useState("");
 
-    return <div className="flex-1 flex justify-center">
-        <div className="w-full max-w-xl">
-            <label htmlFor="session-search" className="sr-only">
-                Search sessions
-            </label>
-            <div className="relative">
-                <input
-                    id="session-search"
-                    value={query}
-                    onChange={(e) => setQuery(e.target.value)}
-                    placeholder="Search sessions, tags or sites"
-                    className="input input-bordered w-full pl-10"
-                />
-                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none z-5">
-                    <CiSearch className="h-5 w-5 text-gray-400" aria-hidden="true" />
-                </div>
-            </div>
+    return <div className="flex-1 flex justify-center w-150">
+        <div className="w-full max-w-3xl">
+            <Input
+                type="text"
+                placeholder="Search sessions, tags, or sites..."
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                className=""
+            />
         </div>
     </div>
 }
