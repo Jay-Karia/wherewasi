df5c39b3894cec5a74bfaaf939e69eddcebee872
Jay-Karia <jay.sanjay.karia@gmail.com>
2025-10-11 03:06:51 +0530

    fix: total tabs recorded stats in popup js

--- popup/popup.js
+++ popup/popup.js
@@ -21,11 +21,9 @@ document.getElementById('openDashboard').addEventListener('click', () => {
 */
 chrome.storage.local.get('sessions', data => {
   const sessions = data.sessions || [];
-  const totalTabs = sessions.reduce((sum, s) => sum + s.tabCount, 0);
 
   document.getElementById('stats').innerHTML = `
-    <strong>${sessions.length}</strong> sessions saved<br>
-    <strong>${totalTabs}</strong> total tabs captured
+    <strong>${sessions.length}</strong> session(s) saved<br>
   `;
 });
 
