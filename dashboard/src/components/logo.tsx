633526635bf3c9a4014eb7fe13f261e8351f6ea2
Jay-Karia <jay.sanjay.karia@gmail.com>
2025-10-17 17:11:38 +0530

    fxi: dashboard logo now displaying in the extension build

--- dashboard/src/components/logo.tsx
+++ dashboard/src/components/logo.tsx
@@ -1,9 +1,7 @@
-import "../../public/logo.png";
-
 export default function Logo() {
   return (
     <div className="transition-all duration-200 hover:opacity-70 hover:rotate-12">
-      <img src="../../public/logo.png" height={30} width={30} alt="Logo" />
+      <img src="../../../assets/logo.png" height={30} width={30} alt="Logo" />
     </div>
   );
 }
