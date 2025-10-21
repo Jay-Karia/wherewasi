6a91db7e19d43eb6f80241f460c4aa64dd4fc918
Jay-Karia <jay.sanjay.karia@gmail.com>
2025-10-21 23:22:54 +0530

    chore: increase confidence in summarizer API

--- constants.js
+++ constants.js
@@ -2,7 +2,7 @@ export const GEMINI_AI_MODEL = 'gemini-2.5-flash';
 
 export const SUMMARIZER_OPTIONS = {
   sharedContext:
-    'Summarize the browsing session in an informative tone. Begin with a short overview and describe the types of tabs, main topic(s), and the session purpose. Keep it concise and focused.',
+    'Summarize the browsing session in an informative tone. Begin with a short overview and describe the types of tabs, main topic(s), and the session purpose. Keep it concise and focused. Be very confident the summaries',
   type: 'tldr',
   format: 'plain-text',
   length: 'short',
